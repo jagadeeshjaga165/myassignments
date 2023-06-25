cabcd1d7b2c7ca2a1213a04ace92426c29019985
jagadeeshjaga165 <137669664+jagadeeshjaga165@users.noreply.github.com>
2023-06-25 09:52:21 +0530

    Add files via upload
    day1

---
+++ day1/Class2.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+import org.testng.annotations.IFactoryAnnotation;
+
+public class Class2 { 
+
+	public static void main(String[] args) {
+		// data variable = value 
+		
+	}
+	
+	int  age = 25;
+	long  phoneNumber =9566158465l;
+	float  mileage =24.6f;
+	boolean  carNew = true;
+	String   brandName = "honda";
+	char  logo = 'H';
+			
+			
+	private void syso() {
+		// TODO Auto-generated method stub
+
+	}
+
---
+++ day1/hellow_selenium.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class hellow_selenium {
+	public static void main(String[] args) {
+		System.out.println("hello Team");
+		
+	}
+
+
+	}
+
